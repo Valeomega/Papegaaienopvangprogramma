09e3e08fa54806ad2a03be822d174e37b46064bc
BuildTools <unconfigured@null.spigotmc.org>
2016-11-19 16:02:53 +0100

    Revert "Revert "added the source of the programm""
    This reverts commit 0c9f92483eb87359d2ce36326a025eb4e9db511d.

---
+++ Papegaaienopvangprogramma/Mainmenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using papegaaiencostumclasses;
+
+
+namespace Papegaaienopvangprogramma
+{
+    public partial class Mainmenu : Form
+    {
+        public Mainmenu()
+        {
+            InitializeComponent();
+        }
+
+        public REPO repoload = savingandloading.ReadFromJsonFile<REPO>("C:/papegaaieninfo");
+
+
+
+
+
+
+        private void opslaanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           savingandloading.WriteToJsonFile<REPO>("C:/papegaaieninfo", repoload, true);
+        }
+
+        private void Mainmenu_Load(object sender, EventArgs e)
+        { REPO repo = new REPO();
+            repo = repoload;
+        }
+         private void nieuwePapegaaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Papegaaitoevoegen pape = new Papegaaitoevoegen(repoload);
+            pape.Show();
+
+        }      
+    }
+     }
+    
+
