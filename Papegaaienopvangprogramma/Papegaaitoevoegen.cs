e31faeb85dbc73297157b065f0829cee4a57938b
BuildTools <unconfigured@null.spigotmc.org>
2016-11-19 15:59:55 +0100

    added the source of the programm

---
+++ Papegaaienopvangprogramma/Papegaaitoevoegen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using papegaaiencostumclasses;
+
+namespace Papegaaienopvangprogramma
+{
+    public partial class Papegaaitoevoegen : Form
+    {
+        public Papegaaitoevoegen(REPO repo)
+        {
+            
+            InitializeComponent();
+            reposhared = repo;
+           comboBoxVogelsoort.DataSource=reposhared.locaties.get 
+        }
+        static REPO reposhared;
+        
+        
+    }
+}
